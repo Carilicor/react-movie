845eb36e9670819cf149e2697bfadaa766024dfc
Cari Licor <Carilicor@gmail.com>
2022-10-27 17:17:34 -0400

    movies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import './App.css';
+import MovieList from './components/MovieList';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MovieList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
